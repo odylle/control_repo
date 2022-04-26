42602634825513836bdc076a3b126a946029cdf9
Vincent Kuipers <github@odylle.org>
2022-04-26 09:51:34 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,7 +5,7 @@ node 'ip-172-31-39-87.eu-central-1.compute.internal' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => $fqdn,
+    content => "Welcome to ${fqdn}\n",
   }
 }
 node /^web/ {
