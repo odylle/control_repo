97f99b6ca09a03bbb4424fb76cbbab9e363fdd14
Vincent Kuipers <github@odylle.org>
2022-04-26 09:34:51 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,3 +4,9 @@ node default {
 node 'ip-172-31-39-87.eu-central-1.compute.internal' {
   include role::master_server
 }
+node /^web/ {
+  include role::app_server
+}
+node /^db/ {
+  include role::db_server
+}
